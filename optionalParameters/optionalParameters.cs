ff3a1248716d8880481dd76b6cd1c9ffea82e661
varunmaster <varunmaster95@gmail.com>
2017-09-18 22:47:59 -0400

    updated optional parameters 9/18

--- optionalParameters/optionalParameters.cs
+++ optionalParameters/optionalParameters.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace optionalParameters
 {
@@ -11,12 +12,15 @@ namespace optionalParameters
         public static void Main()
         {
             addNumbers(1, 2);
+            addNumbers(1, 2, 3);
             //addNumbers(1, 2, new object[] {3, 4, 5});
         }
         /// <summary>
         /// 1. here we have a method where we want to add two numbers but there is also a possibility that we want to add more than 2 numbers so we make 
         /// the other parameters optional by putting them in an object array 
         /// </summary>
+        #region addNumbers method
+        //public static void addNumber(int FN, int SN, [Optional] int[] otherNumbs)
         public static void addNumbers(int FN, int SN, params object[] otherNumbs)
         {
             int result = FN + SN;
@@ -28,5 +32,6 @@ namespace optionalParameters
 
             Console.WriteLine("sum = " + result);
         }
+#endregion
     }
 }
