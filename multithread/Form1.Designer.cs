2d39b599667ab326cb2809f60c150ba46d1ce315
varunmaster <varunmaster95@gmail.com>
2017-10-02 23:35:52 -0400

    multithreading

---
+++ multithread/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace multithread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxNumbers = new System.Windows.Forms.ListBox();
+            this.btnTimeConsumingWork = new System.Windows.Forms.Button();
+            this.btnPrintNumbers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxNumbers
+            // 
+            this.listBoxNumbers.FormattingEnabled = true;
+            this.listBoxNumbers.Location = new System.Drawing.Point(41, 161);
+            this.listBoxNumbers.Name = "listBoxNumbers";
+            this.listBoxNumbers.Size = new System.Drawing.Size(219, 212);
+            this.listBoxNumbers.TabIndex = 0;
+            this.listBoxNumbers.SelectedIndexChanged += new System.EventHandler(this.listBoxNumbers_SelectedIndexChanged);
+            // 
+            // btnTimeConsumingWork
+            // 
+            this.btnTimeConsumingWork.Location = new System.Drawing.Point(41, 15);
+            this.btnTimeConsumingWork.Name = "btnTimeConsumingWork";
+            this.btnTimeConsumingWork.Size = new System.Drawing.Size(218, 50);
+            this.btnTimeConsumingWork.TabIndex = 1;
+            this.btnTimeConsumingWork.Text = "Do Time Consuming Work";
+            this.btnTimeConsumingWork.UseVisualStyleBackColor = true;
+            this.btnTimeConsumingWork.Click += new System.EventHandler(this.btnTimeConsumingWork_Click);
+            // 
+            // btnPrintNumbers
+            // 
+            this.btnPrintNumbers.Location = new System.Drawing.Point(41, 87);
+            this.btnPrintNumbers.Name = "btnPrintNumbers";
+            this.btnPrintNumbers.Size = new System.Drawing.Size(218, 50);
+            this.btnPrintNumbers.TabIndex = 2;
+            this.btnPrintNumbers.Text = "Print Numbers";
+            this.btnPrintNumbers.UseVisualStyleBackColor = true;
+            this.btnPrintNumbers.Click += new System.EventHandler(this.btnPrintNumbers_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 403);
+            this.Controls.Add(this.btnPrintNumbers);
+            this.Controls.Add(this.btnTimeConsumingWork);
+            this.Controls.Add(this.listBoxNumbers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxNumbers;
+        private System.Windows.Forms.Button btnTimeConsumingWork;
+        private System.Windows.Forms.Button btnPrintNumbers;
+    }
+}
+
---
+++ multithread/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace multithread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTimeConsumingWork_Click(object sender, EventArgs e)
+        {
+            btnTimeConsumingWork.Enabled = false;
+            btnPrintNumbers.Enabled = false;
+
+            ///<summary>
+            ///1. if we comment out the next two lines and uncomment out the DoTimeConsumingWork(); then what will happen is that when we click on the button 
+            ///"Do Time Consuming Work" the process/thread will be occupied by one of the process, which is the DoTimeConsumingWork method, and the rest of the
+            ///application will be unresponsive. In order to still use the application while it is doing some sort of time consuming work, we should give the
+            ///time consuming work to some other thread so the application is still usable. In order to give it to another thread, we first call another thread
+            ///called the workerThread and then assign it some time consuming work so there is no delay/the application doesn't become unresponsive.
+            ///</summary>
+            Thread workerThread = new Thread(DoTimeConsumingWork);
+            workerThread.Start();
+            //DoTimeConsumingWork();
+
+            btnTimeConsumingWork.Enabled = true;
+            btnPrintNumbers.Enabled = true;
+        }
+
+        private void DoTimeConsumingWork()
+        {
+            Thread.Sleep(5000);
+        }
+
+        private void btnPrintNumbers_Click(object sender, EventArgs e)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                listBoxNumbers.Items.Add(i);
+            }
+        }
+
+        private void listBoxNumbers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
