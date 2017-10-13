83b7cb91ce6cd8eaee5a34407246754f14521472
varunmaster <varunmaster95@gmail.com>
2017-10-12 22:33:55 -0400

    protecting shared resources from concurrent access in multithreading. i gave up half way through so its incomplete. still works but doesnt have all of the info about thread locking

---
+++ protectingSharedResourcesFromConcurrentAccessinMultithreading/protectingSharedResourcesFromConcurrentAccessinMultithreading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace protectingSharedResourcesFromConcurrentAccessinMultithreading
+{
+    class protectingSharedResourcesFromConcurrentAccessinMultithreading
+    {
+        static int total = 0;
+        public static void Main()
+        {
+            AddOneMillion();
+            AddOneMillion();
+            AddOneMillion();
+            Console.WriteLine("Total = " + total);
+        }
+
+        public static void AddOneMillion()
+        {
+            for(int i = 1; i <= 1000000; i++)
+            {
+                total++;
+            }
+        }
+    }
+}
